50a1a2acb1067bd91b380fe1f6fb8321d16788fd
AlexNeugroschl <alexneugroschl@gmail.com>
2024-06-06 00:28:39 -0400

    tried renaming

--- book-library/bookAPI.py
+++ book-library/bookAPI.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from book import Book
+from book_class_model import BookTemplate
 
 
 app = FastAPI() 
@@ -7,21 +7,21 @@ app = FastAPI()
 books = {}
 
 @app.post('/books/')
-async def add_book(book:book.Book):
-    newID = len(books) + 1
+async def add_book(book:BookTemplate):
+    newID = len(books.keys) + 1
     books[newID] = book
     return book
 
 @app.get('/books/')
 def get_books():
-    return list(books.values)
+    return list(books.values())
 
 @app.get('/books/{id}')
 def get_book(id:int):
     return books[id]
 
 @app.put('/books/{id}')
-def update_book(id:int, book:book.Book):
+def update_book(id:int, book:BookTemplate):
     books[id] = book
     return books
 
