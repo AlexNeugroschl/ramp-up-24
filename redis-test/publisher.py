3d8bdf036d04b78ccf0d01cdfe89567d7aeade13
AlexNeugroschl <alexneugroschl@gmail.com>
2024-06-10 11:40:46 -0400

    trying to dockerize

--- redis-test/publisher.py
+++ redis-test/publisher.py
@@ -3,7 +3,7 @@ import redis
 
 app = FastAPI()
 
-redis_client = redis.Redis(host='localhost', port=6379, db=0)
+redis_client = redis.Redis(host='redis', port=6379, db=0)
 
 @app.post("/publish{message}")
 async def publish_message(request:Request, message:str):
