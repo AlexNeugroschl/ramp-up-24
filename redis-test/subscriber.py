6853b0084bfb8b9d947f69b868ede857a65e2b65
AlexNeugroschl <alexneugroschl@gmail.com>
2024-06-10 14:41:41 -0400

    docker compose started

--- redis-test/subscriber.py
+++ redis-test/subscriber.py
@@ -1,6 +1,6 @@
 import redis
 
-redis_client = redis.Redis(host='localhost', port=6379, db=0)
+redis_client = redis.Redis(host='redis', port=6379, db=0)
 
 pubsub = redis_client.pubsub()
 pubsub.subscribe('my_channel')
