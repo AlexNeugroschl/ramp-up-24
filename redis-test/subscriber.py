61e92e71f7f4299fa8180f6edd71161213314862
AlexNeugroschl <alexneugroschl@gmail.com>
2024-06-11 19:22:32 -0400

    done with redis

--- redis-test/subscriber.py
+++ redis-test/subscriber.py
@@ -1,25 +1,6 @@
-# import redis
-
-# redis_client = redis.Redis(host='redis', port=6379, db=0)
-
-# pubsub = redis_client.pubsub()
-# pubsub.subscribe('my_channel')
-
-# print("Subscribed to the 'my_channel' channel...")
-
-# for message in pubsub.listen():
-#     if message['type'] == 'message':
-#         # No need to decode if decode_responses=True is set
-#         print(f"Received message: {message['data']}")
-#     else:
-#         print(f"Received non-message data: {message}")
-import os
 import redis
 
-redis_host = os.getenv('REDIS_HOST', 'redis')
-redis_port = int(os.getenv('REDIS_PORT', 6379))
-
-redis_client = redis.Redis(host=redis_host, port=redis_port, db=0)
+redis_client = redis.Redis(host='redis', port=6379, db=0)
 
 pubsub = redis_client.pubsub()
 pubsub.subscribe('my_channel')
@@ -28,7 +9,7 @@ print("Subscribed to the 'my_channel' channel...")
 
 for message in pubsub.listen():
     if message['type'] == 'message':
-        # Decode the byte string to a regular string
-        print(f"Received message: {message['data'].decode('utf-8')}")
+        # No need to decode if decode_responses=True is set
+        print(f"Received message: {message['data']}")
     else:
         print(f"Received non-message data: {message}")
